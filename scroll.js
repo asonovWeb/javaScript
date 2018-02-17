1030d654564e56fa5ee5721fa986445ceaf6154a
asonovWeb <36515748+asonovWeb@users.noreply.github.com>
2018-02-17 20:20:56 +0300

    site

---
+++ scroll.js
@@ -0,0 +1,36 @@
+/*==================Скролл к якорю=====================*/
+/*=========событие скролла для отслеживания текущей прокрутки при повторном интервале=====*/
+	var scrolMemory = 0;
+	window.onscroll = function(){
+		scrolMemory = window.pageYOffset || document.documentElement.scrollTop;
+	}
+	var menu = document.getElementById('menu');
+	var offsetTopPx,
+	scrollTOBY = 50;
+	var timerId2;
+/*========событие клика на меню сайта========*/
+	menu.addEventListener('click', function(e){
+		e = e || window.event;
+		e.target;
+		var section = document.getElementsByTagName('section');
+		if(e.target.hasAttribute('href')){/*======Делегирование======*/
+			e.preventDefault();
+			for(var i = 0; i < section.length; i++){/*=======Перебор всех тегов Section=======*/
+				if(section[i].id == e.target.getAttribute('href')){/*===получаю отступ с вверху, если id секции совпадает со значением аттрибута href=====*/
+					offsetTopPx = section[i].offsetTop;
+					if(scrolMemory < offsetTopPx){
+							timerId2 = setInterval(function(){
+							window.scrollBy(0, scrollTOBY);
+							delayEto();
+						},10);
+					};
+				};
+			};
+		};
+	},false);
+	function delayEto(){
+		if(scrolMemory >= offsetTopPx - 50){
+			clearInterval(timerId2);
+			console.log('aa')
+		};
+	}
